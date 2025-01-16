1be7ff3e45f5bf0ce638db95edb61bb0dd13c00c
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:11:01 +0000

    chng

---
+++ array/script.js
@@ -0,0 +1,295 @@
+// let marks = [20, 30, 30, 90, 80, 22]
+// console.log(marks)
+// console.log(marks.length)
+// console.log(marks[3])
+
+// let zain=[30, 20, "zainp", 90,  ]
+// console.log(zain.length)
+// console.log(zain)
+
+
+
+// let marks=[50, 40, 90, 66, 88, 55, 55 ]
+
+// for(let inx=0; inx < marks.length; inx++){
+//     console.log(marks[inx])
+// }
+
+
+// let marks=[85, 97, 44, 37, 76, 60]
+// let sum=0 
+// for(let val of marks){
+
+//     sum=sum+val
+// }
+// let avg=sum/6
+// console.log(avg)
+// console.log(sum)
+
+
+// const fruit=["apple", "banana", "mango", "pine"];
+// for(let fruit of fruits){
+//     console.log(fruit)
+// }
+
+// let fname ="alex";
+// for(let char of fname){
+//     console,log(char);
+// }
+
+// let items=[100, 200, 300, 222, 300, 398]
+// let idx=0
+//  for(let val of items){
+//     console.log('let value of ${idx}=${val}')
+//     idx++;
+//  }
+
+
+// function checkEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return number + " is even.";
+//     } else {
+//         return number + " is odd.";
+//     }
+// }
+
+
+// console.log(checkEvenOrOdd(4)); 
+// console.log(checkEvenOrOdd(7)); 
+
+
+// function check(num){
+//     if(num %2===0){
+//         return num + " is even "
+//     }
+
+//  else{
+//     return num + " is odd " 
+// }
+// }
+// console.log(check(5));
+// console.log(check(10))
+
+
+// function check(age){
+//     if(age>=18){
+//         console.log("you are teenager")
+//     }
+//     else if(age<18){
+//         console.log("you are child")
+//     }
+
+// }
+// check(20)
+
+
+
+
+
+
+// let zain=["shah", "zain", "alam" , "ejs", "khan", ]
+// let val = zain.unshift("nazakat")
+// console.log("input", val)
+// let deepo = zain.shift("")
+// console.log("deleted", deepo)
+// let ehsan=["lala", "afridi", "ahmed", "sahid", "nomi"]
+
+// let people = zain.concat(ehsan)
+// console.log(people) 
+// console.log(ehsan)
+// console.log(ehsan.slice(1, 5))
+
+// zain.splice(2, 2, 101, 111,212)
+
+
+
+// let all=[1,2 ,3,  4 ,5 ,6,4,3,5]
+
+// all.splice(3, 4, 111, 212, 323, 323)
+
+
+
+
+                    //function
+// function zain(msg, n){
+//     console.log("wellcome")
+//     console.log(msg, n)
+// }
+
+// zain("hello world", 100)
+// function multi(x, y){
+//     console.log(x*y)
+// }
+// multi(22, 7)
+
+
+
+// function karahi(meat, masala, tomatto, oil, ginger){
+//     console.log(karahi)
+//     console.log("meat", meat)
+//     console.log("masala", masala)
+//     console.log("tomatto", tomatto)
+//     console.log("oil", oil)
+//     console.log("ginger", ginger)
+//     return"briyani is ready"
+// }
+// let dish=karahi("2kg", "1 packet", "1kg", "1cup","2piece")
+// console.log(dish)
+
+
+//       //second function
+
+
+
+// function makingbbq( meat , masala , oil  , salat ,  ){
+//         if(rice === ""){
+//             return "sorry rice is missing"
+//         }
+//         console.log("meat"  , meat);
+//         console.log("masala" , masala) ;
+//         console.log("oil" ,  oil),
+//         console.log("salat" , salat)
+//       return "bbq is ready"
+    
+    
+//     }
+    
+//     var result = makingBiryani("10k" , "1 pack" , "1/2 kg" ,"up to u" )
+//     console.log(result)
+
+
+
+//                  //third function
+
+
+
+// function makechai() {
+    
+//     const milk = "1l";
+//     const salt = "2 supoon"; 
+//     const tea = "1 supoon";
+//     const ginger = "1 piece" ;
+
+//     console.log("add this all to make tea")  
+//     return "Enjoy your chai!";
+// }
+
+
+// console.log(makechai());
+
+
+
+
+// // function makeadish(roti)
+// // {
+// // console.log(roti)
+// // }
+// // makeadish("i cannot")
+
+
+
+
+
+// var allsum =(a, b, x ,y) =>{
+//     return a+b
+// return x*y 
+// }
+//  let this=allsum(3,5,5,7)
+// console.log(this)
+
+// function function("complete"){
+//     if("a","e","i","o","u"){
+//         console.log(is vovle)
+//     }else if
+// }
+
+
+
+
+
+
+
+
+
+// function sum(a,b){
+//     return a+b
+// }
+
+//  var arthematic=sum(function multiply(helllo)){
+  
+// }
+
+// console.log(arthematic)
+
+
+
+
+// function sum(a){
+//     return a;
+// }
+// var operation=sum(function allsum(x)){
+//     console.log("hello")
+// }
+
+
+
+
+
+
+
+
+// function sum(a){
+//     a()
+// }
+// sum(function arthematic("this is an operatio"))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function product(x,y){
+//     s = x+y
+//     return s;
+
+// }
+// var pro = product(3,5)
+// console.log(pro)
+
+// let arrow = (a,b)=> {
+//     return a, b
+// }
+//  var arr=arrow(4,8)
+//  console.log (arr)
+
+
+
+//  let multi=(a,b)=>{
+//    return a*b
+
+//  }
+//  const mmul=multi(3 , 5)
+// console.log(mmul)
+
+
+
+function vowel(str){
+    let count =0
+    for(const char of str){
+        if (
+            char === "a"
+            char=== "e"
+            char === "i"
+        )
+    }
+    console.log ("apna college")
+}
---
+++ cloddd/script.js
@@ -0,0 +1,17 @@
+const student = {
+    fullname: "shahzain",
+    age:20,
+    marks:300,
+    ispass:true,
+}
+
+
+
+const product={
+    name: "ball personalbar",
+    rating:4,
+    price:370,
+    offer:5,                                            
+}
+
+console.log(product)
