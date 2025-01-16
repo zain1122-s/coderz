0f564db7dd4f2f2eded5cb0c51afbb64262c9761
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:27:10 +0000

    chng

---
+++ javascript/script.js
@@ -0,0 +1,70 @@
+
+// var age = prompt("Find your working experiance by age now");
+// if (age <= 1){
+//     console.log("Infancy")   
+// } else if (age >1 && age <= 10){
+//     console.log("primary")
+// } else if (age > 10 && age <=15){
+//     console.log("secondary")
+// } else if (age >15 && age <=18){
+//     console.log("intermediate")
+// } else if (age >18 && age <=40){
+//     console.log("proplayer")
+// } else if (age >40 && age <=66){
+//     console.log("boss")
+// } else if (age >66 && age <=130){
+//     console.log("super boss")
+// } else if (age >= 130){
+//     console.log("Invalid age")
+// }
+
+
+
+
+
+
+// let days = 356567
+// switch(days) {
+//     case 0 :
+//         console.log("Sunday");
+//     break;
+//     case 1 :
+//         console.log("Monday")
+//     break;   
+//     case 2 :
+//         console.log("Tuesday")
+//     break;   
+//     case 3 :
+//         console.log("Wednesday")
+//     break;   
+//     case 4 :
+//         console.log("Thursday")
+//     break;   
+//     case 5 :
+//         console.log("Friday")
+//     break;   
+//     case 6 :
+//         console.log("saturday")
+//     break;
+//     default :
+//      console.log("Invalid day number")
+// }
+
+let a=3;
+let b=5;
+console.log("a+b=", a+b)
+
+
+
+let i=10
+let j=10
+
+console.log("i*j= " , i*j ,"i%j=", i%j)
+
+console.log("i**j= " , i**j ,"i%j=", i%j)
+
+
+let s=3
+let z=6
+s/=4
+console.log("s=", s )
