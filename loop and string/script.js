365090df986bd82ab02974c65319922596d7f1d9
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:26:20 +0000

    mian

---
+++ loop and string/script.js
@@ -0,0 +1,51 @@
+// loop
+
+
+// for(let i=1; i<=5 ; i++){
+//     console.log("apna college")
+// }
+
+
+    // let sum=0
+
+    // for(let i=1; i<=50; i++){
+    //     sum=sum+i;
+    
+    // }
+    // console.log("sum=", sum )
+
+    // 
+    //let i=1;
+    // while(i<=5){
+    //     console.log("i=", i);
+    //     i++;
+    // }
+
+    // let zain="iamgonemade"
+    // let size=0
+
+    // for(let i of zain){
+    //     console.log("i=", i)
+    //     size++
+    // }
+    // console.log("size", size)
+
+
+    // let student ={
+    //     name :"zain",
+    //     age:20,
+    //     gpa:8,
+    //     ispass:true
+    //     };
+    //     for (let i in student){
+    //         console.log(i , student[i])
+    //     }
+
+    
+
+
+//    let i=9
+//        if(i%2==0 ) {
+//         console.log("even")
+//        }else("odd")
+    
