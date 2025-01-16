f7faefeecd1cf7c17c2edfdf84e0497130716cb0
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:18:39 +0000

    origin

---
+++ dom/script.js
@@ -0,0 +1,39 @@
+// document.body.childNodes[1].innerText="green";
+// document.body.style.background="blue"
+
+
+// let heading=document.getElementById("heading");
+// console.log(heading)
+
+
+// let headings=document.getElementsByClassName("head");
+
+// console.log(headings)
+// console.dir(headings)
+
+
+// let element=document.querySelector("h1");
+// console.dir(element)
+
+
+
+let header = document.querySelector("#main");
+header.innerText = "Hello! JavaScript learners";
+header.style.color = "green";
+
+
+let para = document.querySelector("#paraa");
+para.innerText = "i change the text wow!";
+para.style.fontSize = "18px";
+para.style.color="blue";
+para.style.background="purple"
+
+let footer = document.querySelector("#down");
+footer.innerText = "Thank you for visiting!"
+footer.style.backgroundColor = "red"
+
+// let info=document.querySelector("a");
+// info.a.color("black")
+
+let last=document.querySelector(".para2");
+last.style.backgroundColor="gray"
---
+++ dom/zain.js
@@ -0,0 +1,52 @@
+// console.log("hello")
+// alert("apna college")
+// // let  a= 10
+// // {
+// //     let num =10 ;
+// //     {
+// //         console.log(num)
+// //         {
+// //           var num =10
+// //         }
+// //     }
+// // }
+// // Math object
+// // const user = Math
+// // console.log(Math)
+// // console.log(Math.abs(1))
+// // console.log(Math.cbrt(10))
+// // console.log(Math.ceil(4.1))
+// // console.log(Math.floor(4.7))
+// // console.log(Math.round(4.5))
+// // Math.pow(10 , 2)
+// // Math.max(1,2,3,4)e
+// // console.log(Math.floor(Math.random()*10))  //   0 --> 1 math.floor(0.35345345)
+// const user  = {
+//     name: "John Doe",
+//     age: 30,
+//     city: "New York",
+//     greet: function() {
+//         console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`)
+//     }
+// }
+// user.name ="Lorem"
+// // console.log(user)
+// // user.greet()
+// // _______________________________
+// // {}.propertyName()
+// // let heading = document.querySelector("h1");
+// let heading = document.querySelector(".headone")
+// // heading.innerText = "India"
+// heading.style.backgroundColor = "blue"
+// heading.style.border = "10px solid red"
+// // console.log(heading.innerText)
+// const para = document.querySelector("#paraone");
+// para.innerText = "NOthing"
+// para.style.fontSize = "30px";
+// para.style.color = "green";
+// var age=9;
+// if(age<10){
+//     para.innerText ="GOOD"
+// }else{
+//     para.innerText = "BAD"
+// }
