e5b65cafb047e9637ac931b420cb732ec8ad7672
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:27:34 +0000

    chng

---
+++ java script switch/script.js
@@ -0,0 +1,39 @@
+let months = 5;
+switch(months){
+    case 0:
+        console.log("January");
+    break;
+    case 1:
+        console.log("February")
+    break;
+    case 2:
+        console.log("March")
+    break;
+    case 3:
+        console.log("April")
+    break;
+    case 4:
+        console.log("May")
+    break;
+    case 5:
+        console.log("June")
+    break;
+    case 6:
+        console.log("July")
+    break;
+    case 7:
+        console.log("August")
+    break;
+    case 8:
+        console.log("September")
+    break;
+    case 9:
+        console.log("October")
+    break;
+    case 10:
+        console.log("November")
+    break;
+    case 11:
+        console.log("December")
+    break;
+}
