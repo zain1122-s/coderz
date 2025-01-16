45d14c3f11c3c267748ac8ba3a7c9721dcdd9131
shahzain alam <shahzaina354@gmail.com>
2025-01-16 16:26:39 +0000

    main

---
+++ loop/script.js
@@ -0,0 +1,115 @@
+
+
+
+// let a=4
+// let b=5
+// let c=3
+// console.log("a+b+c=", a+b+c )
+
+// var i=10
+// var j=2
+// console.log("i+j=", i+j )
+
+
+// let age=10
+
+// for(let s=1; s<=10; s++){
+//     console.log(age*s)
+// }
+
+
+// let age=13
+// if(age>=18){
+//     console.log("you are an adult")
+// }else(age>17){
+//     console.log("you are child")
+// }
+
+// let i=5
+// while(i){
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+// let i=4;
+
+// do{
+//         console.log(i)
+//         i++;
+
+// }while(i<5)
+
+
+
+// let year = 5;
+
+// if(year>=18){
+//     console.log("you are adult")
+// }else if(year<18){
+//     console.log("you are children")
+// }
+
+
+let age=14
+if (age>=18){
+    console.log("you are adult")
+}else if(age>=13){
+    console.log("you are teenager")
+}else{
+    console.log("you are child")
+}
+
+
+
+let day="tuesday";
+
+switch (day){
+    case"monday":
+    console.log("this is monday")
+    break;
+    case"tuesday":
+    console.log("this is tuesday")
+    break;
+    case"wednesday":
+    console.log("this is wednesday")
+    break;
+    case"thursday":
+    console.log("this is thursday")
+    break;
+     case"friday":
+    console.log("this is friday")
+    break;
+}
+
+// for(let i=0; i<5; i++){
+//     console.log(i)
+// }
+
+for(let a=1; a<10; a++){
+    console.log(a)
+}
+
+
+
+// let i= 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 5);
+
+
+
+
+// var num= [0, 1, 2, 3]
+// console.log(num)
+
+
+
+let fruit=["apple", "mango", "banana", "orange"]
+console.log(fruit[3])
+
+
+
