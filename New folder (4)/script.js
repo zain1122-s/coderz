3a0d3b0007b224fc857b8c7cc784f13240383a80
shahzain alam <shahzaina354@gmail.com>
2025-01-17 13:36:26 +0000

    chamge

---
+++ New folder (4)/script.js
@@ -0,0 +1,29 @@
+alert("hello word")
+
+
+console.log("hello friend ")
+console.log("hello")
+console.log(2 + 4)
+
+var a = prompt("enter your number")
+
+var istrue = confirm("you comuter is blasting")
+
+if (istrue){
+    console.log("you computer is blastng")
+}
+ else{
+    console.log("you computer is not blast")
+ }
+
+console.log("your number is"   +   a)
+
+
+var a = 6;
+var b = 7;
+ 
+console.log(a + b)
+
+
+
+>=  =====>
